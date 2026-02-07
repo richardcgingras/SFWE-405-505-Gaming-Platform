b9cf295e323dbaf7d8e9d3f53ca2dc5f1ec68be7
Richard C Gingras Jr <richardcgingrasjr@protonmail.com>
2026-02-06 21:53:11 -0500

    adding basic Spring Boot

---
+++ src/main/java/com/example/gaming_platform/GamingPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.example.gaming_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamingPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamingPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gaming_platform/GamingPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gaming_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamingPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
