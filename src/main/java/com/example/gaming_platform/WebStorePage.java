1b4c962edd82cb7a133899d6606f144c3d46a99f
Ernesto Martinez <netojr334@gmail.com>
2026-02-16 12:50:17 -0700

    Created Web Store Page Entity

---
+++ src/main/java/com/example/gaming_platform/WebStorePage.java
@@ -0,0 +1,50 @@
+package com.example.gaming_platform;
+import jakarta.persistence.*;
+import java.util.List;
+
+@Entity
+public class WebStorePage {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+    private String description;
+
+    @Lob
+    private byte[] imageUrl;
+
+    @OneToOne
+    private VideoGame game;
+
+    @ManyToMany
+    private List<VideoGame> developerRecommendations;
+
+    public WebStorePage(){}
+
+    public WebStorePage(String name, String description, byte[] imageUrl,
+                        List<VideoGame> developerRecommendations){
+        this.name = name;
+        this.description = description;
+        this.imageUrl = imageUrl;
+        this.developerRecommendations = developerRecommendations;
+    }
+
+    //setters
+    public void setId(Long id) { this.id = id; }
+    public void setDescription(String description) { this.description = description; }
+    public void setDeveloperRecommendations(List<VideoGame> developerRecommendations) {
+        this.developerRecommendations = developerRecommendations;
+    }
+    public void setGame(VideoGame game) { this.game = game; }
+    public void setImageUrl(byte[] imageUrl) { this.imageUrl = imageUrl; }
+    public void setName(String name) { this.name = name; }
+
+    //Getters
+    public Long getId() { return id; }
+    public String getName() { return name; }
+    public byte[] getImageUrl() { return imageUrl; }
+    public String getDescription() { return description; }
+    public VideoGame getGame() { return game; }
+    public List<VideoGame> getDeveloperRecommendations() { return developerRecommendations; }
+}
