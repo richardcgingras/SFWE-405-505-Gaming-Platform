ccd5fdcc39a99bae1abf0f1b9612d171e424dc54
eharris2 <166444973+eharris2@users.noreply.github.com>
2026-02-17 11:55:49 -0700

    Create VideoGameRepository

---
+++ src/main/java/com/example/gaming_platform/repository/VideoGameRepository.java
@@ -0,0 +1,17 @@
+package com.example.gaming_platform.entity;
+
+import com.example.gaming_platform.entity.VideoGame;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+import java.util.Date;
+
+public interface VideoGameRepository extends CrudRepository<VideoGame, Long> {
+    VideoGame findByName(String name);
+
+    VideoGame findByReleaseDateBefore(Date releaseDate);
+
+    VideoGame findByReleaseDateAfter(Date releaseDate);
+
+    List<VideoGame> findAllByCategory(Category category);
+}
